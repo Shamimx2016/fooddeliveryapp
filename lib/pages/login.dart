a278848b53d93f810095147a267f823109245a3c
shamim <shamimbd621@gmail.com>
2023-11-16 21:49:09 +0600

    new

---
+++ lib/pages/login.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/widget/widget_support.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xffff5c30),
+                    Color(0xffe74b1a),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin:
+                  EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: const Text(''),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 60, left: 20, right: 20),
+              child: Column(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Material(
+                    borderRadius: BorderRadius.circular(20),
+                    elevation: 5,
+                    child: Container(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            'Login',
+                            style: AppWidget.headlineTextFeildStyle(),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              hintStyle: AppWidget.semiboldTextFeildStyle(),
+                              prefixIcon: const Icon(Icons.email_outlined),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle: AppWidget.semiboldTextFeildStyle(),
+                              prefixIcon: const Icon(Icons.password_outlined),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.topRight,
+                            child: Text(
+                              'Forget Password',
+                              style: AppWidget.semiboldTextFeildStyle(),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 80,
+                          ),
+                          Material(
+                            elevation: 5,
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              width: 200,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xffff5722),
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: const Center(
+                                child: Text(
+                                  'LOGIN',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  Text(
+                    "Don't have an account? Sign Up",
+                    style: AppWidget.semiboldTextFeildStyle(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
